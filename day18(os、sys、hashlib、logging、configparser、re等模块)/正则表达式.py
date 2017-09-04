b48279c2293153bb6ac79089d2f37a1894290585
LambertlLan <landingyu@163.com>
2017-09-04 18:30:35 +0800

    20天模块开始

---
+++ day18(os、sys、hashlib、logging、configparser、re等模块)/正则表达式.py
@@ -0,0 +1,7 @@
+# __author: Lambert
+# __date: 2017/9/4 11:04
+import re
+
+ret = re.findall('alex', 'fjsdkfjsdalexfdsfjlsalexfjsdlf')
+
+print(ret)  # ['alex', 'alex']
---
+++ day18(os、sys、hashlib、logging、configparser、re等模块)/配置文件模块.py
@@ -0,0 +1,18 @@
+# __author: Lambert
+# __date: 2017/9/4 10:59
+import configparser
+
+config = configparser.ConfigParser()
+config["DEFAULT"] = {'ServerAliveInterval': '45',
+                     'Compression': 'yes',
+                     'CompressionLevel': '9'}
+
+config['bitbucket.org'] = {}
+config['bitbucket.org']['User'] = 'hg'
+config['topsecret.server.com'] = {}
+topsecret = config['topsecret.server.com']
+topsecret['Host Port'] = '50022'  # mutates the parser
+topsecret['ForwardX11'] = 'no'  # same here
+config['DEFAULT']['ForwardX11'] = 'yes'
+with open('example.ini', 'w') as configfile:
+    config.write(configfile)
